84d97d8cd13f127d180915ba788f09f9fb4c03db
Afshan Akbar <afshanakbar@afshans-mbp.home>
2021-01-29 09:36:54 +0500

    push request

--- app/AdminModel.php
+++ app/AdminModel.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-// comment for pull reqs
+// comment for pull reqsfgfd
 class AdminModel extends Model
 {
     private $firstName, $lastName, $email, $password, $phonoNumber;

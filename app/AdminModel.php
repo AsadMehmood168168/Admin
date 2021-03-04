263a09d9acfe7ba009fe80f1eb77c07b1ea6e1a2
maxwellsci <maxwell.sci.corp@gmail.com>
2021-03-05 02:12:39 +0500

    change test

--- app/AdminModel.php
+++ app/AdminModel.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 // commit from Afshan Again
-// Change from Asad again
+// Change from Asad Mirza
 // commit from Afshan
 // comment for pull reqsfgfd
 class AdminModel extends Model

0f296a68eb9d4791849f56292baddf23db367db0
maxwello <maxwello@maxwellopen.com>
2021-03-15 10:16:23 +0500

    change from Ashi

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -21,45 +21,47 @@ class AdminController extends Controller
     }
     public function login(Request $request)
     {
-        $admin = new AdminModel();
-        $api   = new APIModel();
-        // get request from Api with unique user_email & password
-        $admin->setEmail($request->input('email'));
-        $admin->setPassword($request->input('password'));
-        $admin_login = json_encode($admin->loginJsonSerialize());
-        $api->setUrl("http://localhost:5000");
-        $url = '/admin/' . $admin_login;
-        $data  = $api->getData($url);
-        if (isset($data)) {
-            $admin->setFirstName($data->name);
-            $admin->setEmail($data->email);
-            $admin->setPhonoNumber($data->phoneNumber);
-            $admin->setPassword($data->password);
+        return view('AdminPages.adminHomePage');
+        // $admin = new AdminModel();
+        // $api   = new APIModel();
+        // // get request from Api with unique user_email & password
+        // $admin->setEmail($request->input('email'));
+        // $admin->setPassword($request->input('password'));
+        // $admin_login = json_encode($admin->loginJsonSerialize());
+        // $api->setUrl("http://localhost:5000");
+        // $url = '/admin/' . $admin_login;
+        // $data  = $api->getData($url);
+        // if (isset($data)) {
+        //     $admin->setFirstName($data->name);
+        //     $admin->setEmail($data->email);
+        //     $admin->setPhonoNumber($data->phoneNumber);
+        //     $admin->setPassword($data->password);
 
-            $request->session()->put('admin_Data', $admin);
-            $admin_content = json_encode($admin->jsonSerialize());
-            $decoded_Data  = json_decode($admin_content);
+        //     $request->session()->put('admin_Data', $admin);
+        //     $admin_content = json_encode($admin->jsonSerialize());
+        //     $decoded_Data  = json_decode($admin_content);
 
-            return view('AdminPages.adminHomePage', ["admin_Data" => $decoded_Data]);
-        }
+        //     return view('AdminPages.adminHomePage', ["admin_Data" => $decoded_Data]);
+        // }
     }
     public function store(Request $request)
     {
-        $admin = new AdminModel();
-        $admin->setFirstName($request->input('firstName'));
-        $admin->setLastName($request->input(('lastName')));
-        $admin->setPhonoNumber($request->input(('phonoNumber')));
-        $admin->setEmail($request->input('email'));
-        $admin->setPassword($request->input(('password')));
-        $admin_content = json_encode($admin->jsonSerialize());
-        $api = new APIModel();
-        $api->setUrl("http://localhost:5000");
-        $status = $api->insertData('/admin', $admin_content);
-        if ($status == '200') {
-            $request->session()->put('admin_Data', $admin);
-            $decoded_Data  = json_decode($admin_content);
-            return view('AdminPages.adminHomePage', ["admin_Data" => $decoded_Data]);
-        } 
+        return view('AdminPages.adminHomePage');
+        // $admin = new AdminModel();
+        // $admin->setFirstName($request->input('firstName'));
+        // $admin->setLastName($request->input(('lastName')));
+        // $admin->setPhonoNumber($request->input(('phonoNumber')));
+        // $admin->setEmail($request->input('email'));
+        // $admin->setPassword($request->input(('password')));
+        // $admin_content = json_encode($admin->jsonSerialize());
+        // $api = new APIModel();
+        // $api->setUrl("http://localhost:5000");
+        // $status = $api->insertData('/admin', $admin_content);
+        // if ($status == '200') {
+        //     $request->session()->put('admin_Data', $admin);
+        //     $decoded_Data  = json_decode($admin_content);
+        //     return view('AdminPages.adminHomePage', ["admin_Data" => $decoded_Data]);
+        // } 
     }
     public function show($id)
     {

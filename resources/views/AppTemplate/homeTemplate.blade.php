31418b21bf108b9e28c7bb9caf29b8e3b180160e
maxwellsci <maxwell.sci.corp@gmail.com>
2021-04-01 15:02:52 +0500

    changes in Add Jorunal Blade

--- resources/views/AppTemplate/homeTemplate.blade.php
+++ resources/views/AppTemplate/homeTemplate.blade.php
@@ -12,6 +12,11 @@
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/style.css">
     <style>
       .main{
         margin-top: 10px;
@@ -50,8 +55,16 @@
     </style>
   </head>
   <body>
-    <section>
-      @yield('content')
-    </section>
+    <div class="wrapper d-flex align-items-stretch">
+      <section>
+        @yield('content')
+      </section>
+    </div>
+    {{-- Scripts --}}
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      <script src="js/main.js"></script>
+    {{-- Scripts --}}
   </body>
 </html>
